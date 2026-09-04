7eefa648e32916aa4453e9b6ff94450251453220
Flávio Wilian <wilianf3247@gmail.com>
2026-09-04 11:44:55 -0300

    feat: Criando lógica das rotas

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import cors from 'cors'
 import { errorMiddleware } from './middlewares/error.middleware';
+import clientsRouter from './routes/clients.router';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(clientsRouter);
 app.use(errorMiddleware);
 
 export default app;
