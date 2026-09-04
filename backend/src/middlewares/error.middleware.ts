bb8a0b9d6433d87a861b843a44b61943a5bff155
Flávio Wilian <wilianf3247@gmail.com>
2026-09-04 14:53:07 -0300

    fix: Ajustando o tratamento do retorno do erro.menssage da error.middlewere.ts

--- backend/src/middlewares/error.middleware.ts
+++ backend/src/middlewares/error.middleware.ts
@@ -3,6 +3,14 @@ import { Request, Response, NextFunction } from 'express';
 export const errorMiddleware = ( error: Error, req: Request, res: Response, next: NextFunction ): void => {
   console.error(error);
 
+  if (error.message === 'CPF/CNPJ já cadastrado') {
+    res.status(409).json({
+      erro: error.message,
+    });
+
+    return;
+  }
+
   res.status(500).json({
     erro: 'Erro interno do servidor',
   });
