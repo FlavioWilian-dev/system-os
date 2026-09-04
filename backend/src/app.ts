01fc83855e904a186b63c81039ab8b313957ede5
Flávio Wilian <wilianf3247@gmail.com>
2026-09-04 11:47:00 -0300

    feat: Criando lógica de descoberta automática das rotas

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,14 @@
 import express from 'express';
-import cors from 'cors'
+import cors from 'cors';
+
 import { errorMiddleware } from './middlewares/error.middleware';
-import clientsRouter from './routes/clients.router';
+import routes from './routes';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(clientsRouter);
+app.use(routes);
 app.use(errorMiddleware);
 
 export default app;
