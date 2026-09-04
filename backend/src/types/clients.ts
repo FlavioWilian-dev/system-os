3f3f9fae859bd9bfbfa9cdfe9bee414ad1539e0e
Flávio Wilian <wilianf3247@gmail.com>
2026-09-04 11:17:16 -0300

    feat: Criando typs de clients

---
+++ backend/src/types/clients.ts
@@ -0,0 +1,13 @@
+
+export interface CriarCliente {
+  nomeCliente: string;
+  cpfCnpj: string;
+  fone?: string;
+  email?: string;
+  endereco?: string;
+  bairro?: string;
+  numero?: string;
+  cep?: string;
+  cidade?: string;
+  estado?: string;
+}
