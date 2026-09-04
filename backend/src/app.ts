bf0310644a817e8dcdd8aaee6b396db47d4e9741
Flávio Wilian <wilianf3247@gmail.com>
2026-09-04 10:43:07 -0300

    feat: Registrando o middlewere de erro no app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
-import cors from 'cors';
+import cors from 'cors'
+import { errorMiddleware } from './middlewares/error.middleware';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(errorMiddleware);
 
 export default app;
