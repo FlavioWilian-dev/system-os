75d22ebfd329e6c53b7a2001a97b7e347312baa5
Flávio Wilian <wilianf3247@gmail.com>
2026-09-08 14:44:55 -0300

    feat: Criando router de produtos

---
+++ backend/src/routes/products.router.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+import { criarProduto, obterProdutos } from '../modules/products/products.controller';
+import { validar } from '../middlewares/validate.middleware';
+import { criarProdutoSchema } from '../schemas/products.schema';
+
+const router = Router();
+
+router.post(
+    '/',
+    validar(criarProdutoSchema),
+    criarProduto
+);
+
+router.get(
+    '/',
+    obterProdutos
+)
