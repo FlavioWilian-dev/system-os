0bde324ed5f32df15d2fe50172395fc4c3996f49
Flávio Wilian <wilianf3247@gmail.com>
2026-09-08 16:24:35 -0300

    fix: ajustando retorno do controller de produtos

--- backend/src/modules/products/products.controller.ts
+++ backend/src/modules/products/products.controller.ts
@@ -17,7 +17,7 @@ export const obterProdutos = async ( req: Request, res: Response, next: NextFunc
   try {
     const produtos = await produtosService.obterProdutos(req.body);     
 
-    res.status(200).json({ message: 'Produtos obtidos com sucesso' });
+    res.status(200).json({ message: 'Produtos obtidos com sucesso', data: produtos });
     } catch (error) {
     res.status(500).json({ message: 'Erro ao obter produtos' });
     next(error);
